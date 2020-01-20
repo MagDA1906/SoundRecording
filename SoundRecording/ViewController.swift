e2086cfc209e665eac2cf021c45e068d3a93b843
Денис Магильницкий <magilnitsky@gmail.com>
2020-01-20 23:35:19 +0300

    start

---
+++ SoundRecording/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundRecording
+//
+//  Created by Денис Магильницкий on 20/01/2020.
+//  Copyright © 2020 Денис Магильницкий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
